bd085a8ce56c75d8a34511464142d3ea7ffc5ea8
EasyGoingLove <50263561+EasyGoingLove@users.noreply.github.com>
2021-05-24 06:14:53 +0300

    Add files via upload

--- variant2/upr3.java
+++ variant2/upr3.java
@@ -8,22 +8,19 @@ public class upr3 {
         String encodedString = scanner.next();
         System.out.println("Enter an encoding String:");
         String encodingString = scanner.next();
-        
+        // Example input : zaqxswcdevfrbgtnhymjukilop
         correctInputLoop(encodingString,scanner);
-
-        
-        
-        
-        
+        solution(encodingString,encodedString);
 
     }
+
     static void correctInputLoop(String encodingString,Scanner scanner) {
         while (true) {
             if(checkForRepeat(encodingString)){
                 System.out.println("Letters must not repeat in the input 'encoding String'");
                }
             else if(checkForLetter(encodingString)==false) {
-                System.out.println("Encoding String must contain only letters");
+                System.out.println("Encoding String must contain only letters and must be 26 letter long");
                }
             else{
                 break;
@@ -45,6 +42,9 @@ public class upr3 {
 
     static boolean checkForLetter(String encoderString) {
         boolean checker = true;
+        if (encoderString.length()<26) {
+            checker = false;
+        }else{
         for (int i = 0; i < encoderString.length(); i++){
             if( (encoderString.charAt(i)>= 'a' && encoderString.charAt(i) <= 'z') || (encoderString.charAt(i)>= 'A' && encoderString.charAt(i)<= 'Z')){
                 checker = true;
@@ -53,7 +53,39 @@ public class upr3 {
                 break;
             }
         }  
+    }
         return checker;
     }
-    
+    static void solution(String encodingString, String encodedString) {
+        int alphabetPossition = 1;
+        char alphabet[] = new char[26];
+        int array[] = new int[encodedString.length()];
+        int arrayCounter = 0;
+        char codedMsg[] = new char[100];
+
+        for(char c = 'a'; c <= 'z'; c++){
+           alphabet[arrayCounter] = c;
+           arrayCounter++;
+        }
+        arrayCounter=0;
+
+        for (int i = 0; i < encodedString.length(); i++) {
+            for (int v = 0; v < alphabet.length; v++) {
+                if (encodedString.charAt(i) == alphabet[v]) {
+                    array[arrayCounter] = v;
+                    arrayCounter++;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int v = 0; v < encodingString.length(); v++) {
+                if(array[i] == v){
+                    codedMsg[i] =encodingString.charAt(v);
+                }
+          }
+        }
+        String str = String.valueOf(codedMsg);
+        System.out.println(str);
+    }
 }
+
